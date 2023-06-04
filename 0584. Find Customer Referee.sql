30910c36381f02f9c96d790f0d3995e24810acaf
Madhur Agarwal <85205360+AgarwalMaddy@users.noreply.github.com>
2023-06-04 08:46:20 +0530

    0584. Find Customer Referee.sql

--- 0584. Find Customer Referee.sql
+++ 0584. Find Customer Referee.sql
@@ -1,5 +1,5 @@
-// 584. Find Customer Referee 
-// https://leetcode.com/problems/find-customer-referee/description/?envType=study-plan-v2&envId=top-sql-50
+# 584. Find Customer Referee 
+# https://leetcode.com/problems/find-customer-referee/description/?envType=study-plan-v2&envId=top-sql-50
 
 # Write your MySQL query statement below
 SELECT name 

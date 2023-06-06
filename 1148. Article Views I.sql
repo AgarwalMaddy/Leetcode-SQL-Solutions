f9bfd15225a1ed7fe46f3a1b9a2431b77e06bfda
Madhur Agarwal <85205360+AgarwalMaddy@users.noreply.github.com>
2023-06-06 21:22:18 +0530

    1148. Article Views I.sql

---
+++ 1148. Article Views I.sql
@@ -0,0 +1,9 @@
+# 1148. Article Views I
+# https://leetcode.com/problems/article-views-i/?envType=study-plan-v2&envId=top-sql-50
+
+# Write your MySQL query statement below
+SELECT DISTINCT(author_id) AS id  
+FROM Views 
+WHERE author_id = viewer_id
+GROUP BY article_id
+ORDER BY id ASC;
